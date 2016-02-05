f0ddf1ec86b8e54fc9bbc40b60402a0d8471b79d
killercrush <falovskii-artur@mail.ru>
2016-02-05 16:16:48 +0500

    first stage

--- script.js
+++ script.js
@@ -14,41 +14,43 @@ var lam1 = 1 / 30,
     t = 30;
 
 var t1 = [
-  {count: 1, val: 40, disp: 15},
-  {count: 2, val: 29, disp: 11},
-  {count: 3, val: 22, disp: 8}
+  //{workers_count: 1, duration: 40, disp: 15},
+  {workers_count: 1, duration: 4, disp: 1},
+  {workers_count: 2, duration: 29, disp: 11},
+  {workers_count: 3, duration: 22, disp: 8}
 ];
 
 var t2 = [
-  {count: 2, val: 40, disp: 10},
-  {count: 3, val: 25, disp: 17},
+  {workers_count: 2, duration: 40, disp: 10},
+  {workers_count: 3, duration: 25, disp: 17},
 ];
 
 var t3 = [
-  {count: 1, val: 28, disp: 9},
-  {count: 2, val: 20, disp: 8},
+  {workers_count: 1, duration: 28, disp: 9},
+  {workers_count: 2, duration: 20, disp: 8},
 ];
 
 var t4 = [
-  {count: 1, val: 40, disp: 10},
-  {count: 2, val: 30, disp: 7},
-  {count: 3, val: 20, disp: 5},
-  {count: 4, val: 16, disp: 4}
+  {workers_count: 1, duration: 40, disp: 10},
+  {workers_count: 2, duration: 30, disp: 7},
+  {workers_count: 3, duration: 20, disp: 5},
+  {workers_count: 4, duration: 16, disp: 4}
 ];
 
-var minutes = 0,
-    prev_arriv1 = 0,
-    prev_arriv2 = 0;
-var uz1_queue = [];
-var uz2_queue = [];
-var adjust_queue = [];
-var assemble_queue = [];
-var complete_queue = [];
+
 
 function Unit(id, price, time_arriv) {
   this.id = id;
   this.price = price;
   this.time_arriv = time_arriv;
+  var st1, st2, st3 = false;
+  this.status = function() {
+    return "Unit #" + id + " status: " +
+      st1 + ", " + st2 + ", " + st3;
+  }
+  this.print = function() {
+    return "Unit #" + id;
+  }
 }
 
 
@@ -62,31 +64,34 @@ function Product(u1, u2) {
 }
 
 // Операция подгонки
-function FirstStage(len) { 
+function FirstStage(params, in1, in2, out1, out2) { 
   this.in_process = false;
   this.timer = 0;
   this.unit1 = {};
   this.unit2 = {};
-  this.start = function (uz1, uz2) {
-    if (uz1.length == 0 || uz2.length == 0 || this.in_process) {
-      return;
-    }
-    this.timer = len.val + getRandomInt(-len.disp, len.disp);
-    this.unit1 = uz1.pop();
-    this.unit2 = uz2.pop();
-    console.log(this.unit1);
-    console.log(this.unit2);
-    this.in_process = true;
-  }
   this.process = function() {
     if (this.in_process) {
       this.timer--;
       if (this.timer == 0) {
         this.in_process = false;
-        return new Product(this.unit1, this.unit2);
+        out1.push(this.unit1);
+        out2.push(this.unit2);
+        console.log(this.unit1.print() + " is finished stage 1");
+        console.log(this.unit2.print() + " is finished stage 1");
+        // this.unit1 = {};
+        // this.unit2 = {};
       }
-      else return false;
+      return;
+    }   
+    if (in1.length == 0 || in2.length == 0) {
+      return;
     }
+    this.timer = params.duration + getRandomInt(-params.disp, params.disp);
+    this.unit1 = in1.shift();
+    this.unit2 = in2.shift();
+    console.log(this.unit1.print() + " is on stage 1");
+    console.log(this.unit2.print() + " is on stage 1");
+    this.in_process = true;
   }
 }
 
@@ -147,49 +152,53 @@ function ThirdStage(len) {
   }
 }
 
+var minutes = 0,
+    prev_arriv1 = 0,
+    prev_arriv2 = 0;
+var unit_queue1 = [];
+var unit_queue2 = [];
+var after_st1_queue1 = [];
+var after_st1_queue2 = [];
 
-var first_stage = new FirstStage(t1[0]);
+var adjust_queue = [];
+var assemble_queue = [];
+var complete_queue = [];
+
+var first_stage = new FirstStage(t1[0], unit_queue1, unit_queue2, after_st1_queue1, after_st1_queue2);
 var second_stage = new SecondStage(p1, t2[0], p1, t3[0], p2);
 var third_stage = new ThirdStage(t4);
 
 while (true) {
   minutes++;
-  if (minutes % 10 == 0) {//if (minutes - prev_arriv1 > 1 / lam1) {
+  if (minutes < 5) {//if (minutes - prev_arriv1 > 1 / lam1) {
     prev_arriv1 = minutes;
-    uz1_queue.push(new Unit('d1_' + minutes, s1, minutes));
+    unit_queue1.push(new Unit('u1_' + minutes, s1, minutes));
   }
-  if (minutes % 10 == 0) {//if (minutes - prev_arriv2 > 1 / lam2) {
+  if (minutes < 5) {//if (minutes - prev_arriv2 > 1 / lam2) {
     prev_arriv2 = minutes;
-    uz2_queue.push(new Unit('d2_' + minutes, s1, minutes));
+    unit_queue2.push(new Unit('u2_' + minutes, s1, minutes));
   }
 
-  first_stage.start(uz1_queue, uz2_queue);
-  var op_res = first_stage.process();
-  if (op_res){
-    op_res.time_fo = minutes;
-    adjust_queue.push(op_res);
-    console.log(op_res);
-  }
+  first_stage.process();
 
-  var adj_res = second_stage.start(adjust_queue) || second_stage.process(adjust_queue);
-  if (adj_res) {
-    adj_res.time_adj = minutes;
-    console.log(adj_res);
-    assemble_queue.push(adj_res);
-  }
+  // var adj_res = second_stage.start(adjust_queue) || second_stage.process(adjust_queue);
+  // if (adj_res) {
+  //   adj_res.time_adj = minutes;
+  //   console.log(adj_res);
+  //   assemble_queue.push(adj_res);
+  // }
 
-  third_stage.start(assemble_queue);
-  var asm_res = third_stage.process(assemble_queue);
-  if (asm_res) {
-    asm_res.time_asm = minutes;
-    complete_queue.push(asm_res);
-  }
+  // third_stage.start(assemble_queue);
+  // var asm_res = third_stage.process(assemble_queue);
+  // if (asm_res) {
+  //   asm_res.time_asm = minutes;
+  //   complete_queue.push(asm_res);
+  // }
 
   if (minutes > 59) break;
 }
-console.log("uz1_queue: ", uz1_queue);
-console.log("uz2_queue: ",uz2_queue);
-console.log("adjust_queue: ", adjust_queue);
-console.log("assemble_queue: ", assemble_queue);
-console.log("complete_queue: ", complete_queue);
+console.log("unit_queue1: ", unit_queue1);
+console.log("unit_queue2: ", unit_queue2);
+console.log("after_st1_queue1: ", after_st1_queue2);
+console.log("after_st1_queue1: ", after_st1_queue2);
 })();
